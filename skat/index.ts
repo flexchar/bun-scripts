233ed83bf09e98a824027c45abec48deef39d24f
Lukas <git@lukasnotes.dk>
2024-07-06 11:11:46 +0200

    update logic

--- skat/index.ts
+++ skat/index.ts
@@ -17,58 +17,108 @@ if (!TO_DATE) {
 
 const csvString = await (await fetch(SHEETS_CSV_URL)).text();
 
-const records = parse(csvString, {
+const initialRecords = parse(csvString, {
     cast: true,
-    cast_date: true,
+    // cast_date: true,
     columns: true,
     skip_empty_lines: true,
 });
 
-type Record = {
+type InitialRecord = {
     invoiceId: string;
-    date: Date;
+    date: string;
     name: string;
     type: 'A - Services' | 'A - Goods' | 'B - Services';
-    inEu: boolean;
-    inDk: boolean;
+    inEu: 0 | 1;
+    inDk: 0 | 1;
     grandTotal: number;
     vatRate: number;
     baseValue: number;
     vatValue: number;
 };
 
-// filter records from January 1st, 2024 to March 31st, 2024
-const filteredRecords = records.filter((r: Record) => {
-    const date = r.date;
-    return date >= new Date(FROM_DATE) && date <= new Date(TO_DATE);
+// Verify types using zod
+import { z } from 'zod';
+const recSchema = z.object({
+    invoiceId: z.any(),
+    date: z.string().transform((d) => new Date(d)),
+    name: z.string(),
+    type: z.enum(['A - Services', 'A - Goods', 'B - Services']),
+    inEu: z
+        .number()
+        .min(0)
+        .max(1)
+        .transform((v) => Boolean(v)),
+    inDk: z
+        .number()
+        .min(0)
+        .max(1)
+        .transform((v) => Boolean(v)),
+    grandTotal: z.number(),
+    vatRate: z.number(),
+    baseValue: z.number(),
+    vatValue: z.number(),
 });
 
+type Record = z.infer<typeof recSchema>;
+
+const filteredRecords = initialRecords
+    // Filter records for the period defined in .env
+    .filter((r: InitialRecord) => {
+        const date = new Date(r.date);
+        return date >= new Date(FROM_DATE) && date <= new Date(TO_DATE);
+    })
+    .map((r: InitialRecord) => {
+        try {
+            const p = recSchema.parse(r);
+            return p;
+        } catch (e) {
+            console.error(e.errors);
+            console.error(r);
+            process.exit(1);
+        }
+    });
 // console.log(filteredRecords);
 // process.exit(0);
+
+// Info
+console.info(
+    `Found ${filteredRecords.length} entries for the ${FROM_DATE} - ${TO_DATE} period.`,
+);
 
 // Caclulate
 // https://skat.dk/en-us/businesses/vat/vat-on-international-trade/reporting-your-international-trade
 // https://skat.dk/erhverv/moms/moms-ved-handel-med-udlandet/indberet-din-handel-med-udlandet
+// New links because SKAT broke them once again
+// https://skat.dk/en-us/businesses/vat/vat-on-international-trade/vat-on-sales-to-businesses/reporting-your-international-trade
+// https://skat.dk/erhverv/moms/moms-ved-handel-med-udlandet/moms-ved-handel-med-virksomheder/indberet-din-handel-med-udlandet
 const tax = {
     'vat-in-dk': 0, // Moms i DK til Købsmoms (Input VAT) (VAT deductible)
+
+    // You calculate the VAT as 25% of the invoice value of the services purchased during the period.
+    // This will be included in the Købsmoms (Input VAT) (VAT deductible)
+    'manual-25-reverse-charge': 0, // manually added 25% on reverse charge
 
     // VAT on goods purchased outside Denmark (both the EU and third countries).
     'vat-on-goods-purchased-outside-denmark': 0,
     // VAT on services purchased outside Denmark subject to a reverse charge
-    'vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge': 0,
     // to calculate Købsmoms (Input VAT) (VAT deductible)
+    'vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge': 0,
+
+    // to improve visibility, let's split in EU and outside EU
     'vat-on-services-purchased-outside-denmark-outside-eu': 0,
+    'vat-on-services-purchased-outside-denmark-inside-eu': 0,
 
     'eu-sales-with-vat': 0,
     'eu-sales-without-vat': 0,
 
-    'vat-paid': 0, // Købsmoms
-    'vat-collected': 0, // Salgsmoms
+    'vat-paid': 0, // Købsmoms (Input VAT) (VAT deductible)
+    'vat-collected': 0, // Salgsmoms (Output VAT) (VAT payable)
 
     // Boxes (base value here excl. VAT)
     'box-a-services': 0,
     'box-a-goods': 0,
-    // Not relevant for my business
+
     'box-b-services': 0,
     'box-b-goods': 0,
     'box-c-services': 0,
@@ -78,8 +128,6 @@ tax['vat-in-dk'] = filteredRecords
     .filter((r: Record) => r.inDk)
     .filter((r: Record) => r.type === 'A - Services' || r.type === 'A - Goods')
     .reduce((acc: number, r: Record) => acc + r.vatValue, 0);
-
-let vatOf25estimatedForReverseCharge = 0;
 
 // Moms af varekøb i udlandet (både EU og lande uden for EU)
 // VAT on goods purchased outside Denmark (both the EU and third countries).
@@ -93,7 +141,7 @@ tax['vat-on-goods-purchased-outside-denmark'] = filteredRecords
 
         if (r.vatRate === 0) {
             value = r.grandTotal * 0.25;
-            vatOf25estimatedForReverseCharge += value;
+            tax['manual-25-reverse-charge'] += value;
         }
 
         return acc + value;
@@ -101,22 +149,23 @@ tax['vat-on-goods-purchased-outside-denmark'] = filteredRecords
 
 // Moms af ydelseskøb i udlandet med omvendt betalingspligt
 // VAT on services purchased outside Denmark subject to a reverse charge
-tax['vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge'] =
-    filteredRecords
-        .filter((r: Record) => !r.inDk && r.inEu)
-        // Only report the value of the EU services you purchased in box A - services.
-        .filter((r: Record) => r.type === 'A - Services')
-        .filter((r: Record) => r.vatRate === 0)
-        .reduce((acc: number, r: Record) => {
-            let value = r.vatValue;
+// When you buy services from countries outside Denmark, both in and outside the EU, you must pay VAT on the purchase. You therefore have to calculate and pay Danish VAT on the service yourself.
+// You calculate the VAT (25%) on the value of the service by first converting the value to Danish kroner. Then you multiply by 0.25.
+tax['vat-on-services-purchased-outside-denmark-inside-eu'] = filteredRecords
+    .filter((r: Record) => !r.inDk && r.inEu)
+    // Only report the value of the EU services you purchased in box A - services.
+    .filter((r: Record) => r.type === 'A - Services')
+    .filter((r: Record) => r.vatRate === 0)
+    .reduce((acc: number, r: Record) => {
+        let value = r.vatValue;
 
-            if (r.vatRate === 0) {
-                value = r.grandTotal * 0.25;
-                vatOf25estimatedForReverseCharge += value;
-            }
+        if (r.vatRate === 0) {
+            value = r.grandTotal * 0.25;
+            tax['manual-25-reverse-charge'] += value;
+        }
 
-            return acc + value;
-        }, 0);
+        return acc + value;
+    }, 0);
 
 tax['vat-on-services-purchased-outside-denmark-outside-eu'] = filteredRecords
     .filter((r: Record) => !r.inDk && !r.inEu)
@@ -126,14 +175,14 @@ tax['vat-on-services-purchased-outside-denmark-outside-eu'] = filteredRecords
 
         if (r.vatRate === 0) {
             value = r.grandTotal * 0.25;
-            vatOf25estimatedForReverseCharge += value;
+            tax['manual-25-reverse-charge'] += value;
         }
 
         return acc + value;
     }, 0);
-
-// Print the manually added 25% on reverse charge
-console.info({ vatOf25estimatedForReverseCharge });
+tax['vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge'] =
+    tax['vat-on-services-purchased-outside-denmark-inside-eu'] +
+    tax['vat-on-services-purchased-outside-denmark-outside-eu'];
 
 // Købsmoms // Input VAT (VAT deductible)
 // You may include amounts from the following fields:
@@ -144,8 +193,7 @@ tax['vat-paid'] =
     tax['vat-on-goods-purchased-outside-denmark'] +
     tax[
         'vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge'
-    ] +
-    tax['vat-on-services-purchased-outside-denmark-outside-eu'];
+    ];
 
 // Box A - goods
 // VAT on goods purchased outside Denmark (both the EU and third countries).
@@ -175,6 +223,17 @@ tax['box-b-services'] = filteredRecords
     .filter((r: Record) => r.type === 'B - Services')
     .reduce((acc: number, r: Record) => acc + r.grandTotal, 0);
 
+// Box C - services/goods
+// The value of other goods and services sold exclusive of VAT in Denmark, other EU countries and countries outside the EU, see section 76 of the Executive Order
+// Rubrik C. Værdien af andre varer og ydelser, der leveres uden afgift her i landet, i andre EU-lande og i lande uden for EU.
+// KISS: bascically my sales anywhere that don't have VAT attached, typically B2B
+tax['box-c-services'] = filteredRecords
+    .filter((r: Record) => r.vatRate === 0)
+    .filter((r: Record) => r.type === 'B - Services') // i don't sell goods so only services
+    .reduce((acc: number, r: Record) => acc + r.baseValue, 0);
+// Since sales are registered as negative numbers in my expenses sheet, we need to make them positive
+tax['box-c-services'] = Math.abs(tax['box-c-services']);
+
 // EU sales with VAT
 // The value of certain sales of goods and services exclusive of VAT to other EU countries. To be reported under ‘EU-salg med moms’ (EU sales with VAT)
 // KISS: Sales of goods and services to the EU where I charged VAT (typically B2C)
@@ -192,16 +251,16 @@ tax['eu-sales-without-vat'] = filteredRecords
     .filter((r: Record) => r.vatRate === 0)
     .reduce((acc: number, r: Record) => acc + r.baseValue, 0);
 
-// No sales for me... yet :)
-// console.log(
-//     `
-// Report the value of your sale in two different places in E-tax for businesses:
+tax['eu-sales-without-vat'] &&
+    console.log(
+        `
+Report the value of your sale in two different places in E-tax for businesses:
 
-// In your VAT return within the normal deadlines that apply to your business.
-// Under ’EU-salg uden moms’ (EU sales exclusive of VAT) by the 25th day of each month.
+In your VAT return within the normal deadlines that apply to your business.
+Under ’EU-salg uden moms’ (EU sales exclusive of VAT) by the 25th day of each month.
 
-//     `.trim(),
-// );
+    `.trim(),
+    );
 
 // Round to no decimal places
 Object.keys(tax).forEach((key) => {
@@ -210,18 +269,44 @@ Object.keys(tax).forEach((key) => {
 
 // Add Danish labels, it is easier to know where to put the numbers in the tax form
 const dansk = {
+    Salgsmoms: tax['vat-collected'],
+    Købsmoms: tax['vat-paid'],
+
     'Moms af varekøb i udlandet (både EU og lande uden for EU)':
         tax['vat-on-goods-purchased-outside-denmark'],
     'Moms af ydelseskøb i udlandet med omvendt betalingspligt':
         tax[
             'vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge'
         ],
-    Købsmoms: tax['vat-paid'],
+
     'Rubrik A - varer': tax['box-a-goods'],
     'Rubrik A - ydelser': tax['box-a-services'],
     'Rubrik B - ydelser': tax['box-b-services'],
+    'Rubrik C': tax['box-c-services'],
     'EU-salg med moms': tax['eu-sales-with-vat'],
     'EU-salg uden moms': tax['eu-sales-without-vat'],
 };
 
+// Add English labels, it is easier to know where to put the numbers in the tax form
+const english = {
+    'Output VAT (VAT payable)': tax['vat-collected'],
+    'Input VAT (VAT deductible)': tax['vat-paid'],
+
+    'VAT on goods purchased abroad (both the EU and third countries)':
+        tax['vat-on-goods-purchased-outside-denmark'],
+    'VAT on services purchased abroad subject to a reverse charge':
+        tax[
+            'vat-on-services-purchased-outside-denmark-subject-to-a-reverse-charge'
+        ],
+
+    'Box A - goods': tax['box-a-goods'],
+    'Box A - services': tax['box-a-services'],
+    'Box B - services': tax['box-b-services'],
+    'Box C': tax['box-c-services'],
+    'EU sales with VAT': tax['eu-sales-with-vat'],
+    'EU sales exclusive of VAT': tax['eu-sales-without-vat'],
+};
+
+console.log(tax);
+console.dir(english);
 console.dir(dansk);
